c866abbdd4f038c45aad401083e1ef597d49aae8
Artur Sulej <mr.artur.su@gmail.com>
2025-06-09 17:38:50 +0200

    Working load cell

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_svc::hal::{
+    delay::{Delay, FreeRtos},
+    gpio::PinDriver,
+    peripherals::Peripherals,
+};
 
-    // Bind the log crate to the ESP Logging facilities
+use loadcell::{hx711::HX711, LoadCell};
+
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let dt = PinDriver::input(peripherals.pins.gpio2).unwrap();
+    let sck = PinDriver::output(peripherals.pins.gpio3).unwrap();
+    let delay = Delay::new_default();
+
+    let mut load_sensor = HX711::new(sck, dt, delay);
+
+    load_sensor.tare(16);
+    load_sensor.set_scale(1.0);
+
+    loop {
+        if load_sensor.is_ready() {
+            let reading = load_sensor.read_scaled().unwrap();
+            // let reading = load_sensor.read().unwrap(); // Use this to calibrate the load cell
+            log::info!("Weight: {:.0} g", reading);
+            // log::info!("Weight: {} g", reading); // Use this to get all the decimals
+        }
+
+        FreeRtos::delay_ms(1000u32);
+    }
 }
